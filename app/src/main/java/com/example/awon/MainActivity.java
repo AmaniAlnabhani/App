7f8a0707ff3279146a353109464bd2cf4a36e5a0
Amani AL-Nabhani <53594712+AmaniAlnabhani@users.noreply.github.com>
2022-12-30 19:49:50 +0400

    Initial commit

---
+++ app/src/main/java/com/example/awon/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.awon;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+
+import com.example.awon.databinding.HomeBinding;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    private  static int SPLASH_SCREEN=5000; // mean 5 seconds
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+
+
+}
